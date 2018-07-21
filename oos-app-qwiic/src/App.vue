71998345d402836c68816e715040ccf392699708
Zheng <zh@onion.io>
2018-07-21 12:30:44 -0400

    get data function added

--- oos-app-qwiic/src/App.vue
+++ oos-app-qwiic/src/App.vue
@@ -4,10 +4,13 @@
     <div class="control">
 
       <label class="form-switch float-right">
-        <input type="checkbox" v-model="isAutoRefresh">
+        <input type="checkbox" v-model="isAutoRefresh"
+        :true-value="true"
+        :false-value="false">
         <i class="form-icon"></i>Auto Refresh
       </label>
-      <button class="refreshBtn btn float-right" v-if="!isAutoRefresh">Refresh</button>
+      <button class="refreshBtn btn float-right" v-if="!isAutoRefresh"
+      @click="onRefresh">Refresh</button>
 
     </div>
 
@@ -89,6 +92,35 @@ export default {
       this.isLoading = false
       console.log(command, result)
     }.bind(this)
+
+    setInterval(function(){
+      if (this.isAutoRefresh) {
+        this.getData()
+      }
+    }.bind(this), 1000)
+  },
+  methods: {
+    onRefresh () {
+      this.getData()
+    },
+    getData (deviceAddr, callback) {
+        // data retrieval to be implemented here later
+
+        // hard-coded data for now
+        var data = "     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n00:          -- -- -- -- -- -- -- -- -- -- -- -- --\n10: UU -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n30: 30 31 32 33 34 35 36 37 -- -- -- -- -- -- -- --\n40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n50: 50 51 52 53 54 55 56 57 58 59 5a 5b 5c 5d 5e 5f\n60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --\n70: -- -- -- -- -- -- -- —"
+
+        // callback is the function to:
+        //    parse string
+        //    populate product array
+        // callback(deviceAddr, {
+        //     ch0: a0,
+        //     ch1: a1,
+        //     ch2: a2,
+        //     ch3: a3,
+        // })
+
+        console.log(data)
+    }
   }
 
 }
