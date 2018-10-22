ac1dd8c9b71bf006f60393c28170a0fc667e26de
greenbreakfast <lazardemin@gmail.com>
2018-10-22 20:54:42 +0000

    implemented reading heat values for each pixel on amg8833 device

--- omega/src/amg8833.h
+++ omega/src/amg8833.h
@@ -11,13 +11,14 @@
 #define AMG8833_INTC 		0x03
 
 // constants
-#define AMG8833_PIXEL_TEMP_CONVERSION 	.25
-#define AMG8833_SLEEP_MS 								100
+#define AMG8833_PIXEL_TEMP_CONVERSION 	0.25
+#define AMG8833_SLEEP_MS 				100
+#define AMG8833_NUM_PIXELS              64
 
 
 // functions
 int amg8833_setup();
-int amg8833_readPixels();
+int amg8833_readPixels(float *pixelData);
 
 
 #endif	/* AMG8833_H_INCLUDED */
--- omega/src/main.c
+++ omega/src/main.c
@@ -1,14 +1,26 @@
 #include "amg8833.h"
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	int status = 0;
-
+int amg8833_device() {
+	int status;
+	float *pixelData = malloc(AMG8833_NUM_PIXELS * sizeof(float));
+	
 	status = amg8833_setup();
 
 	if (status == EXIT_SUCCESS) {
-		status = amg8833_readPixels();
+		status = amg8833_readPixels(pixelData);
 	}
+	
+	free(pixelData);
+	return status;
+}
+
+int main(int argc, char *argv[]) {
+	int status = 0;
+	
+	printf("Program version: %s %s\n", __DATE__, __TIME__);
+
+	status = amg8833_device();
 
 
 	return 0;
