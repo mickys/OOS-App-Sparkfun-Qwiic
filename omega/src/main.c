0e1115ecc59d2a52ee0dc9918e146e8fb36e02b8
Micky Socaci <micky@nowlive.ro>
2019-11-28 14:02:40 +0200

    Fixed Variables

--- omega/src/main.c
+++ omega/src/main.c
@@ -100,8 +100,13 @@ int main(int argc, char *argv[]) {
 
 	config_t cfg;
 	config_setting_t *setting;
-	const char* server, identifier, topic, certificate, ds18cmd;
-	int port, time;
+	const char* server;
+	const char* identifier;
+	const char* topic;
+	int port;
+	int time;
+	const char* certificate;
+	const char* ds18cmd;
 
 	config_init(&cfg);
 	/* Read the file. If there is an error, report it and exit. */
